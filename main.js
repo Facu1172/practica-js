d835b99d824c7358e6bb36874923d456c00e43a4
Facu1172 <Facu172@hotmail.com>
2022-08-28 23:54:07 -0300

    Practica JS

---
+++ main.js
@@ -0,0 +1,20 @@
+alert ("Bienvenido")
+alert ("Esta aplicacion permite escribir un mensaje y repetir ese mensaje la cantidad de veces que quieras")
+
+let mensaje = prompt("ingrese su mensaje")
+let x = parseInt(prompt("ingrese en números la cantidad de veces que se van a repetir"))
+
+for (let i = 1; i <= x; i++){
+alert (mensaje);
+}
+
+switch(parseInt(prompt("ingrese una opcion \n 1- Volver al inicio \n 2- salir"))){
+    case 1: alert("presiona F5")
+    break;
+    case 2:alert("Gracias por utilizar esta aplicacion")
+    let opcion = parseInt(prompt ("antes de irte recomendarias esta funcion a tus amigos \n 1-Si \n 2-No"))
+if (opcion === 1){alert("Comparte esta aplicacion!");}
+
+if (opcion === 2){alert("Lamentamos que no te guste, dejanos un comentario para que podamos mejorar");}
+    break;
+}
